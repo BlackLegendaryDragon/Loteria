778cb6840123e46852b08bef824ee3c809bcdf34
NextVaso <pablocurutchet92@gmail.com>
2024-06-03 21:35:26 -0300

    Ventana

---
+++ src/loteria/Game.java
@@ -0,0 +1,16 @@
+package loteria;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel implements Runnable{
+
+    public Game(){
+        setBackground(Color.BLUE);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/loteria/Main.java
+++ src/loteria/Main.java
@@ -2,8 +2,24 @@ package loteria;
 
 import javax.swing.*;
 
-public class Main extends JFrame {
+public class Main extends JFrame
+    {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        new Main();
+    }
+
+    public Main()
+    {
+        Game game = new Game();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Loteria");
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+        add(game);
     }
 }
